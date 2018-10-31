cabe418f03b5de68e7100eed9fa3f8d5269a1a3c
raymond-burrowes <raymond.burrowes@maplink.global>
2018-10-31 14:49:49 +0100

    Formatting of text. Addition & deletion of steps based on actual behaviour. Some assertions commented.

--- droneFlightPlanning.js
+++ droneFlightPlanning.js
@@ -1,22 +1,115 @@
-describe('Successful flight plan map image point insertion', function() {
-	it('Given the application is loaded', function() {
-		browser.get('https://stupendous-birth.surge.sh');
-		browser.sleep(1500);
+describe('Successful flight plan map image point insertion',
+		function() {
+		it('Given the web application launch is complete',
+				function() {
+				browser.get('https://stupendous-birth.surge.sh');
+				browser.sleep(1500);
 
-		// Jasmine assertions
-		expect(element(by.css("h2[class='md-toolbar-item md-breadcrumb md-headline']")).getText()).toBe("Drone Flight Planner")
+				// assertions
+				expect(element(by.css("h2[class='md-toolbar-item md-breadcrumb md-headline']")).getText())
+					.toBe("Drone Flight Planner")
+				expect(element(by.buttonText('Create a new Flight Planadd')).getText())
+					.toBe("CREATE A NEW FLIGHT PLANadd")
+				mapImg = element(by.css("img[src='./assets/screen-shot.png']"))
+				expect(mapImg.isPresent()).toBe(true)
 
-		element(by.css("h2[class='md-toolbar-item md-breadcrumb md-headline']")).getText().then(
-				function(text) {
-					console.log(text);
-				})
-	})
-	it('And the map image is displayed in the main panel', function() {
-	})
-	it('And the left side panel displays all registered flight plans', function() {
-	})
-	it('When the main panel is clicked', function() {
-	})
-	it('Then the first flight plan point is inserted to the clicked location', function() {
-	})
-})
+				// logging
+				element(by.css("h2[class='md-toolbar-item md-breadcrumb md-headline']"))
+					.getText().then(
+						function(titleText) {
+						console.log("Title text is: " + titleText);
+						}
+					)
+				element(by.buttonText('Create a new Flight Planadd'))
+					.getText().then(
+						function(buttonText) {
+						console.log("Button text is: " + buttonText);
+						}
+					)
+				element(by.css("img[src='./assets/screen-shot.png']"))
+					.getText().then(
+						function(imgFile)  {
+						console.log("Map image is: " + imgFile);
+						}
+					)
+				}
+		)
+		it('the Create a new Flight Plan button is clicked',
+				function() {
+				element(by.buttonText('Create a new Flight Planadd')).click();
+				browser.sleep(1500);
+				
+				// assertions
+//				expect(element(by.css("svg[class='leaflet-zoom-animated']")).getAttribute(width))
+//					.toBe("1559")
+//				expect(element(by.css("svg[class='leaflet-zoom-animated']")).getAttribute(height))
+//					.toBe("493")
+						
+				// logging
+//				element(by.css("svg[class='leaflet-zoom-animated']"))
+//					.getAttribute(width).then(
+//						function(mapWidth) {
+//						console.log("Map width is: " + mapWidth);
+//						}
+//					)
+//				element(by.css("svg[class='leaflet-zoom-animated']"))
+//					.getAttribute(height).then(
+//						function(mapHeight) {
+//						console.log("Map height is: " + mapHeight);
+//						}
+//					)
+				}
+		)
+		it('When the main panel is clicked', 
+				function() {
+				}
+		)
+		it('Then the first flight plan point is inserted to the clicked location',
+				function() {
+				}
+		)
+		}
+)
+		
+describe('Successful flight plan web app launch',
+		function() {
+		it('Given the web application is launched',
+				function() {
+				browser.get('https://stupendous-birth.surge.sh');
+				browser.sleep(1500);
+				}
+		)
+		it('Then the elements of the initial web page are loaded',
+				function() {
+		
+				// assertions
+				expect(element(by.css("h2[class='md-toolbar-item md-breadcrumb md-headline']")).getText())
+					.toBe("Drone Flight Planner")
+				expect(element(by.buttonText('Create a new Flight Planadd')).getText())
+					.toBe("CREATE A NEW FLIGHT PLANadd")
+				mapImg = element(by.css("img[src='./assets/screen-shot.png']"))
+				expect(mapImg.isPresent()).toBe(true)
+
+				// logging
+				element(by.css("h2[class='md-toolbar-item md-breadcrumb md-headline']"))
+					.getText().then(
+						function(titleText) {
+						console.log("Title text is: " + titleText);
+						}
+					)
+				element(by.buttonText('Create a new Flight Planadd'))
+					.getText().then(
+						function(buttonText) {
+						console.log("Button text is: " + buttonText);
+						}
+					)
+				element(by.css("img[src='./assets/screen-shot.png']"))
+					.getText().then(
+						function(imgFile)  {
+						console.log("Map image is: " + imgFile);
+						}
+					)
+				}
+		)
+		}
+)
